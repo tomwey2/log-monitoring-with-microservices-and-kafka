1de38df5ebb8caa62593fb4fe7de84906aac8303
Thomas <tomwey2@gmail.com>
2023-11-19 13:30:35 +0400

    updated producer

---
+++ log-rawdata-producer/src/main/java/de/tomwey2/poc/log/producer/Producer.java
@@ -0,0 +1,11 @@
+package de.tomwey2.poc.log.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producer {
+    public static void main(String[] args) {
+        SpringApplication.run(Producer.class, args);
+    }
+}
